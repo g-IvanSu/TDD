7540ee05bbc9396d5c916fce1a2e9ac317732c9c
g-ivansu <g-ivansu@gamania.com>
2023-12-01 23:36:36 +0800

    hahow tdd

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import Session
+from models import Board
+from datetime import datetime
+
+
+def get_ptt_boards():
+    url = "https://www.ptt.cc/bbs/index.html"
+
+    resp = requests.get(url=url)
+    soup = BeautifulSoup(resp.text, "lxml")
+
+    result = []
+    for tmp in soup.find_all(name="div", attrs={"class": "board-name"}):
+        result.append(tmp.text)
+
+    return result
+
+
+def insert_ptt_board(name: str, session: Session):
+    board = Board(
+        name=name,
+        created_time=datetime.now()
+    )
+    session.add(board)
